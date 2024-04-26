cf6e554208a9dae476d90f3416c4874e647ba4db
Qodirov Doston <qodirovdoston806@gmail.com>
2024-04-26 13:29:48 -0400

    create landing page for EG dispatching

--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"hlBzavogr55VBpxC2neJ87yW4DKlJFml9VniX4p23D8=\"\n}"
--- src/app/layout.js
+++ src/app/layout.js
@@ -23,6 +23,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+         <Head>
+          <meta property="og:image" content="https://eg-dispatch-service.vercel.app/_next/image…atic%2Fmedia%2FEGDispatch.9d6e346c.png&w=256&q=75" />
+        </Head>
       <body className={inter.className}>
         <BaseLayout>{children}</BaseLayout>
       </body>
